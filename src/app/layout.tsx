5e57613916bcd9a3303932b821f1739e0c3f3c0d
chawinkn <chawin26112549@gmail.com>
2024-06-01 08:26:06 +0700

    Fix: Datecolor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,27 +26,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const dayColor: { [key: number]: string } = {
-    0: "from-red-700 via-red-500 to-red-300",
-    1: "from-amber-700 via-amber-500 to-amber-300",
-    2: "from-pink-700 via-pink-500 to-pink-300",
-    3: "from-green-700 via-green-500 to-green-300",
-    4: "from-orange-700 via-orange-500 to-orange-300",
-    5: "from-indigo-700 via-indigo-500 to-indigo-300",
-    6: "from-violet-700 via-violet-500 to-violet-300",
-  };
-  const day: number = new Date().getDay();
-  const dateColor = dayColor[day];
-
   return (
     <html lang="en">
-      <body
-        className={clsx(
-          inter.variable,
-          notoSans.variable,
-          `bg-gradient-to-t ${dateColor}`
-        )}
-      >
+      <body className={clsx(inter.variable, notoSans.variable)}>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+"use client";
+
 import Footer from "@/components/Footer";
 import { Event } from "../components/event";
 import CountdownCard from "@/components/CountdownCard";
+import clsx from "clsx";
 
 export default function Home() {
+  const dayColor: Array<string> = [
+    "from-red-700 via-red-500 to-red-300",
+    "from-amber-700 via-amber-500 to-amber-300",
+    "from-pink-700 via-pink-500 to-pink-300",
+    "from-green-700 via-green-500 to-green-300",
+    "from-orange-700 via-orange-500 to-orange-300",
+    "from-indigo-700 via-indigo-500 to-indigo-300",
+    "from-violet-700 via-violet-500 to-violet-300",
+  ];
+  const day: number = new Date().getDay();
+  const dateColor: string = dayColor[day];
+
   return (
     <>
-      <main className="flex items-center justify-center min-h-screen py-10 w-full">
+      <main
+        className={clsx(
+          `bg-gradient-to-t ${dateColor}`,
+          "flex items-center justify-center min-h-screen py-10 w-full"
+        )}
+      >
         <div className="flex flex-col items-center justify-center max-w-md space-y-6">
           <h1 className="text-5xl md:text-6xl font-extrabold mb-6">
             Countdown
